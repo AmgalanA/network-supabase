cf7661f2c71f50cf5a88bc212695f2208d0792d5
Amgalan <amgalan12346@gmail.com>
2022-10-04 22:30:11 +0800

    fixed login component

--- app/components/pages/home/auth/login/Login.tsx
+++ app/components/pages/home/auth/login/Login.tsx
@@ -5,44 +5,7 @@ import Field from "../../../../ui/form-elements/field/Field";
 import styles from "./Login.module.scss";
 
 const Login = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    mode: "onChange",
-  });
-
-  const onSubmit: SubmitHandler<ISignInFields> = async (data) => {
-    const response = await AuthService.signIn(data);
-    console.log(response);
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Login</h1>
-
-      {/* <Field
-        placeholder="E-mail"
-        type="email"
-        error={errors?.email}
-        {...register("email", {
-          required: "Please, enter an e-mail",
-        })}
-      />
-
-      <Field
-        placeholder="password"
-        type="password"
-        error={errors?.password}
-        {...register("password", {
-          required: "Please, enter an password",
-        })}
-      /> */}
-
-      <button type="submit">Sign In</button>
-    </form>
-  );
+  return <div>Login</div>;
 };
 
 export default Login;
